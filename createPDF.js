e988b5ddcc7182b95a08b025ec8e20139099ab09
Ruben Vasquez <ruvasqm@gmail.com>
2022-11-09 08:18:00 -0400

    fix(root): bump node version, add puppeteer args, bump actions/core version

--- createPDF.js
+++ createPDF.js
@@ -1,16 +1,31 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
 
 async function createPDF(url, args = {}) {
-  try{
-    const browser = await puppeteer.launch({ headless: true,  ...args });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: {
+        deviceScaleFactor: 1,
+        hasTouch: false,
+        height: 1080,
+        isLandscape: true,
+        isMobile: false,
+        width: 1920,
+      },
+      ...args,
+    });
     const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'networkidle0'});
-    const pdf = await page.pdf({ format: 'A4' });
+    await page.goto(url, { timeout: 10000, waitUntil: "networkidle0" });
+    const pdf = await page.pdf({
+      format: "a4",
+      displayHeaderFooter: false,
+      printBackground: true,
+    });
     await browser.close();
     return pdf;
-    }catch(err){
-      console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-module.exports = createPDF
+module.exports = createPDF;
--- dev.js
+++ dev.js
@@ -2,7 +2,7 @@ const main = require("./createPDF");
 const fs = require("fs").promises
 
 async function run(){
-    const pdf = await main("https://imbhargav5.com");
+    const pdf = await main("https://ruvasqm.tech/resume");
     await fs.writeFile('output.pdf',pdf);
     return pdf  
 }
